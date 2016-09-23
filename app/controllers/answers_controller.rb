32d181df1ee656c87453af5e1bb6ce739d0e3655
epicatization <radeksnara@gmail.com>
2016-09-24 00:16:41 +0200

    add answer actions in controller, to handle events in frontend

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,74 +1,45 @@
 class AnswersController < ApplicationController
-  before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
-  # GET /answers
-  # GET /answers.json
   def index
-    @answers = Answer.all
+    @answers = question.answers
   end
 
-  # GET /answers/1
-  # GET /answers/1.json
-  def show
+  def valid
+    ActionCable.server.broadcast('familiada',
+      action: 'ADD_VALID_ANSWER',team: team, points: points
+    )
+    head :ok
   end
 
-  # GET /answers/new
-  def new
-    @answer = Answer.new
+  def invalid
+    ActionCable.server.broadcast('familiada',
+      action: 'ADD_INVALID_ANSWER', team: team
+    )
+    head :ok
   end
 
-  # GET /answers/1/edit
-  def edit
-  end
-
-  # POST /answers
-  # POST /answers.json
-  def create
-    @answer = Answer.new(answer_params)
-
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render :show, status: :created, location: @answer }
-      else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /answers/1
-  # PATCH/PUT /answers/1.json
-  def update
-    respond_to do |format|
-      if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
-      else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /answers/1
-  # DELETE /answers/1.json
-  def destroy
-    @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def clear
+    ActionCable.server.broadcast('familiada',
+      action: 'CLEAR_INVALID_ANSWERS'
+    )
+    head :ok
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_answer
-      @answer = Answer.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def answer_params
-      params.require(:answer).permit(:title, :points, :question_id)
-    end
+  def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  def answer
+    @answer ||= Answer.find(params[:id])
+  end
+
+  def team
+    params[:team]
+  end
+
+  def points
+    answer.points
+  end
 end
