48495cad26f349b816934720838c4349bda380cb
epicatization <radeksnara@gmail.com>
2016-09-24 00:15:02 +0200

    adjust question controller and model with answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,11 @@
 class Question < ApplicationRecord
+  has_many :answers
+  def next
+    self.class.where("id > ?", id).first
+  end
+
+  def previous
+    self.class.where("id < ?", id).last
+  end
+
 end
